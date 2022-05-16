a9169110c1f2fb6738e75cc1a943eb23e5004334
jobcher <13028911306@163.com>
2022-05-16 14:18:10 +0800

    第一次更新

---
+++ 1.py
@@ -0,0 +1,15 @@
+#第一个python
+help(print)
+#打印换行
+print("hello python")
+print("job cher")
+print("hello world\ntest")
+print('''hello world 
+        python''')
+#打印颜色
+print("\033[31;1;31m hello world\033[0m")
+print("\033[31;1;32m hello world\033[0m")
+print("\033[31;1;33m hello world\033[0m")
+print("\033[31;1;34m hello world\033[0m")
+print("\033[31;1;35m hello world\033[0m")
+print("\033[31;1;36m hello world\033[0m")
